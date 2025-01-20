63dbf5be071114b575a3d368cb8f0c88039fe51c
Nguyen Xuan Quang <quang456a@gmail.com>
2025-01-20 21:03:11 +0700

    complete edit post

--- app/(admin)/dashboard/draft-posts/page.tsx
+++ app/(admin)/dashboard/draft-posts/page.tsx
@@ -8,26 +8,28 @@ import { Post } from '@/types';
 import { getPostsByStatus } from '../../actions/getPosts';
 
 const DraftPosts = () => {
-	const [postList, setPostList] = useState<Post[]>([]);
+  const [postList, setPostList] = useState<Post[]>([]);
 
-	useEffect(() => {
-		const getDraftPosts = async () => {
-			const result = await getPostsByStatus('draft');
-			if (result) {
-				setPostList(result as Post[]);
-			}
-		};
-		getDraftPosts();
-	}, []);
+  useEffect(() => {
+    const getDraftPosts = async () => {
+      const result = await getPostsByStatus('draft');
+      if (result) {
+        setPostList(result as Post[]);
+      }
+    };
+    getDraftPosts();
+  }, []);
 
-	return (
-		<div className="flex flex-col">
-			<ContentHeader leftsideTitle="Bài viết" rightsideTitle="nháp" />
-			<div className="mt-10">
-				<PostsTable columns={PostsTableColumns} data={postList} />
-			</div>
-		</div>
-	);
+  return (
+    <div className="flex flex-col">
+      <ContentHeader leftsideTitle="Bài viết"
+        rightsideTitle="nháp" />
+      <div className="mt-10">
+        <PostsTable columns={PostsTableColumns}
+          data={postList} />
+      </div>
+    </div>
+  );
 };
 
 export default DraftPosts;
